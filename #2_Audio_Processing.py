f0c78bd11427fa2b093fe56ceced3a1279607bad
Shivam Waghela <shivamwaghela@gmail.com>
2016-11-26 16:08:25 +0530

    refactor: implemented concise code for notes identification

--- #2_Audio_Processing.py
+++ #2_Audio_Processing.py
@@ -15,10 +15,18 @@ import wave
 import struct
 
 ############################## Initialize ##################################
-# Some Useful Variables
 window_size = 1000#2205  # Size of window to be used for detecting silence
 sampling_freq = 44100  # Sampling frequency of audio signal
-
+L = [("A0", 27.50), ("A1", 55.00), ("A2", 110.00), ("A3", 220.00), ("A4", 440.00), ("A5", 880.00), ("A6", 1760.00),
+     ("A7", 3520.00), ("A8", 7040.00), ("B0", 30.87), ("B1", 61.74), ("B2", 123.47), ("B3", 246.94), ("B4", 493.88),
+     ("B5", 987.77), ("B6", 1975.53), ("B7", 3951.07), ("B8", 7902.13), ("C0", 16.35), ("C1", 32.70), ("C2", 65.41),
+     ("C3", 130.81), ("C4", 261.63), ("C5", 523.25), ("C6", 1046.50), ("C7", 2093.00), ("C8", 4186.01),
+     ("D0", 18.35), ("D1", 36.71), ("D2", 73.42), ("D3", 146.83), ("D4", 293.66), ("D5", 587.33), ("D6", 1174.66),
+     ("D7", 2349.32), ("D8", 4698.63), ("E0", 20.60), ("E1", 41.20), ("E2", 82.41), ("E3", 164.81), ("E4", 329.63),
+     ("E5", 659.25), ("E6", 1318.51), ("E7", 2637.02), ("E8", 5274.04), ("F0", 21.83), ("F1", 43.65), ("F2", 87.31),
+     ("F3", 174.61), ("F4", 349.23), ("F5", 698.46), ("F6", 1396.91), ("F7", 2793.83), ("F8", 5587.65), ("G0", 24.50),
+     ("G1", 49.00), ("G2", 98.00), ("G3", 196.00), ("G4", 392.00), ("G5", 783.99), ("G6", 1567.98), ("G7", 3135.96),
+     ("G8", 6271.93)]
 ############################# Implementation ###############################
 def play(sound_file):
     '''
@@ -54,69 +62,31 @@ def play(sound_file):
         if mean_squared_list[i] < 0.1:  # If amplitude mean-square value < threshold then silence
             silence.append(i)  # store the index of window of silence
 
-    # if silence is at the beginning of the audio
-    i = 0
-    if silence[0] == 0:  # if silence[0] == 0 then block 0 is silence
-        while True:
-            if silence[i + 1] == silence[i] + 1:
-                i += 1
-            else:
-                break
-
-        start = (i + 1) * (window_size)
-        end = silence[i + 1] * window_size
-    else:
-        start = 0
-        end = silence[0] * window_size
-
-    exi = 0
-    #    i = 0
     notes_freq = []
 
-    for iterator in range(len(silence)):
+    if silence[0] != 0:
+        start = 0
+        end = silence[0] * window_size
         fourier = np.fft.fft(sound[start:end])
         freqs = np.fft.fftfreq(len(fourier))
-        idx = np.argmax(np.abs(fourier))
-        freq = freqs[idx]
-        frate = sound_file.getframerate()
-        freq_in_hertz = abs(freq * frate)
-
+        max_index = np.argmax(np.abs(fourier))
+        freq = freqs[max_index]
+        freq_in_hertz = abs(freq * sampling_freq)
         notes_freq.append(freq_in_hertz)
 
-        i += 1
-
-        if (end == file_length - 1): break
-
-        while True:
-            if (len(silence) - 1 != i and i < len(silence)):  # to prevent index out of bound error
-
-                if silence[i + 1] != silence[i] + 1:  # if adjacent blocks are not silence
-                    start = (silence[i] + 1) * window_size
-                    end = silence[i + 2] * window_size
-                    i += 1
-                    break
-                else:  # if adjacent is also silence
-                    i += 1
-            else:
-                start = (silence[i] + 1) * window_size
-                end = file_length - 1
-                break
-
-
-
+    for index, element in enumerate(silence):
+        if index < len(silence) - 1:
+            if silence[index + 1] - element > 1:
+                start = element * window_size
+                end = silence[index + 1] * window_size
+                fourier = np.fft.fft(sound[start:end])
+                freqs = np.fft.fftfreq(len(fourier))
+                max_index = np.argmax(np.abs(fourier))
+                freq = freqs[max_index]
+                freq_in_hertz = abs(freq * sampling_freq)
+                notes_freq.append(freq_in_hertz)
 
     # Identify notes from frequency
-    L = [("A0", 27.50), ("A1", 55.00), ("A2", 110.00), ("A3", 220.00), ("A4", 440.00), ("A5", 880.00), ("A6", 1760.00),
-         ("A7", 3520.00), ("A8", 7040.00), ("B0", 30.87), ("B1", 61.74), ("B2", 123.47), ("B3", 246.94), ("B4", 493.88),
-         ("B5", 987.77), ("B6", 1975.53), ("B7", 3951.07), ("B8", 7902.13), ("C0", 16.35), ("C1", 32.70), ("C2", 65.41),
-         ("C3", 130.81), ("C4", 261.63), ("C5", 523.25), ("C6", 1046.50), ("C7", 2093.00), ("C8", 4186.01),
-         ("D0", 18.35), ("D1", 36.71), ("D2", 73.42), ("D3", 146.83), ("D4", 293.66), ("D5", 587.33), ("D6", 1174.66),
-         ("D7", 2349.32),("D8", 4698.63), ("E0", 20.60), ("E1", 41.20), ("E2", 82.41), ("E3", 164.81), ("E4", 329.63),
-         ("E5", 659.25), ("E6", 1318.51), ("E7", 2637.02), ("E8", 5274.04), ("F0", 21.83), ("F1", 43.65), ("F2", 87.31),
-         ("F3", 174.61), ("F4", 349.23), ("F5", 698.46), ("F6", 1396.91), ("F7", 2793.83),("F8", 5587.65), ("G0", 24.50),
-         ("G1", 49.00), ("G2", 98.00), ("G3", 196.00), ("G4", 392.00), ("G5", 783.99), ("G6", 1567.98), ("G7", 3135.96),
-         ("G8", 6271.93)]
-
     Identified_Notes = []
     freq = [x[1] for x in L]
     note_name = [x[0] for x in L]
@@ -131,9 +101,9 @@ def play(sound_file):
 ############################## Testing Audio File #############################
 if __name__ == "__main__":
     #code for checking output for all audio
-    for file_number in range(1,4):
+    for file_number in range(1, 6):
         Identified_Notes = []
-        file_name = "/media/shivam/644C84384C840750/Users/Swaminarayan/Documents/eYantra 2016/Task 1/Audio_Processing/Audacity/Audio_" + str(
+        file_name = "/Users/Swaminarayan/Documents/eYantra 2016/Task 1/Audio_Processing/Test_Audio_files/Audio_" + str(
             file_number) + ".wav"
         sound_file = wave.open(file_name)
         Identified_Notes = play(sound_file)
